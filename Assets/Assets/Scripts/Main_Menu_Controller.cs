9e866524a39684d9168e01981759d090e82873e3
gokmentas <56593104+gokmentas@users.noreply.github.com>
2023-10-06 19:06:59 +0300

    Done

---
+++ Assets/Assets/Scripts/Main_Menu_Controller.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu_Controller : MonoBehaviour
+{
+    public void playerOneStart(){
+        SceneManager.LoadScene(1);
+    }
+}
